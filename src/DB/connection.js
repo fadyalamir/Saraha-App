01740219755f3b6918a2073f1def30b9dd0de022
Fady Alamir <fadyalamer72@gmail.com>
2026-01-03 18:30:46 +0200

    update the project

--- src/DB/connection.js
+++ src/DB/connection.js
@@ -1,11 +1,26 @@
+// import mongoose from "mongoose";
+
+// const connectDB = async () => {
+//   await mongoose.connect(process.env.DB_URI).then(res => {
+//     console.log(`DB connected`);
+//   }).catch (err => {
+//     console.error(`Fail to connect on DB`, err);
+//   })
+// }
+
+// export default connectDB;
+
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  await mongoose.connect(process.env.DB_URI).then(res => {
-    console.log(`DB connected`);
-  }).catch (err => {
-    console.error(`Fail to connect on DB`, err);
-  })
-}
+  try {
+    const conn = await mongoose.connect(process.env.DB_URI);
+    
+    console.log(`DB Connected: ${conn.connection.host}`);
+  } catch (err) {
+    console.error(`Fail to connect on DB:`, err.message);
+    throw err; 
+  }
+};
 
 export default connectDB;
